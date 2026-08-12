abeb949860b74d7d17a0888676695f90823ac341
Brandon ospina <sazory1810@gmail.com>
2026-08-12 18:52:09 -0500

    feat: agrego funcion suma hecha por Brandon

---
+++ suma.py
@@ -0,0 +1,10 @@
+#funcion de sumar hecha por Brandon
+
+def suma(a, b):
+    return a + b
+
+int1 = int(input("Ingrese el primer número: "))
+int2 = int(input("Ingrese el segundo número: "))        
+
+resultado = suma(int1, int2)
+print(f"El resultado de la suma es: {resultado}")
